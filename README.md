b1b2e3bee3573f182fac06a7238cc56709a5dcd6
carolynworldwide <carolynworldwide@gmail.com>
2015-10-01 13:53:05 -0700

    updated README and fixed code style

---
+++ README.md
@@ -0,0 +1,4 @@
+Driver pulling Carolyn
+Navigator pulling Noah
+
+We worked on this code together and it worked!
--- game.js
+++ game.js
@@ -10,47 +10,47 @@ var three = document.getElementById('three');
 
 function question1(){
 	gender = prompt('Am i a boy?');
-genderCorrectAnswer = 'yes'
-gender=gender.toLowerCase()
-if(gender === genderCorrectAnswer||gender ==='y'){
-	correctAnswers += 1
-	one.innerHTML = 'HEYO, good guess'
-	//alert('HEYO, good guess!');
-}else if(gender === 'n'||gender ==='no'){
-	one.innerHTML = 'no, my hair\'s not that long is it?'
-	//alert('no, my hair\'s not that long is it?');
-}
+	genderCorrectAnswer = 'yes'
+	gender=gender.toLowerCase()
+	if(gender === genderCorrectAnswer||gender ==='y'){
+		correctAnswers += 1
+		one.innerHTML = 'HEYO, good guess'
+		//alert('HEYO, good guess!');
+	}else if(gender === 'n'||gender ==='no'){
+		one.innerHTML = 'no, my hair\'s not that long is it?'
+		//alert('no, my hair\'s not that long is it?');
+	}
 }
 question1();
 
 function question2(){
 	birthPlace = prompt('Was I born in seattle?');
-birthPlaceCorrectAnswer = 'yes'
-birthPlace=birthPlace.toLowerCase()
-if(birthPlace === birthPlaceCorrectAnswer||birthPlace === 'y'){
-	correctAnswers += 1
-	two.innerHTML = 'how\'d you know?'
-	//alert('how\'d you know?');
-}else if(birthPlace === 'n'||birthPlace ==='no'){
-	two.innerHTML = 'hmm, not quite'
-	//alert('hmm, not quite');
-}
+	birthPlaceCorrectAnswer = 'yes'
+	birthPlace=birthPlace.toLowerCase()
+	if(birthPlace === birthPlaceCorrectAnswer||birthPlace === 'y'){
+		correctAnswers += 1
+		two.innerHTML = 'how\'d you know?'
+		//alert('how\'d you know?');
+	}else if(birthPlace === 'n'||birthPlace ==='no'){
+		two.innerHTML = 'hmm, not quite'
+		//alert('hmm, not quite');
+	}
 }
 question2();
 
 
 function question3(){
 	favoriteFood = prompt('Is Pho my favorite food?');
-favoriteFoodCorrectAnswer = 'yes'
-favoriteFood=favoriteFood.toLowerCase()
-if(favoriteFood === favoriteFoodCorrectAnswer||favoriteFood === 'y'){
-	correctAnswers += 1
-	three.innerHTML = 'Correct! So yummy!'
-	//alert('Correct! So yummy!');
-}else if(favoriteFood === 'n'||favoriteFood ==='no'){
-	three.innerHTML = 'Of course it is!'
-	//alert('Of course it is!');
-}
+	favoriteFoodCorrectAnswer = 'yes'
+	favoriteFood=favoriteFood.toLowerCase()
+	if(favoriteFood === favoriteFoodCorrectAnswer||favoriteFood === 'y'){
+		correctAnswers += 1
+		three.innerHTML = 'Correct! So yummy!'
+		//alert('Correct! So yummy!');
+	}else if(favoriteFood === 'n'||favoriteFood ==='no'){
+		three.innerHTML = 'Of course it is!'
+		//alert('Of course it is!');
+	}
 }
 question3();
 
