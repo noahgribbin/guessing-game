b64be8b295200dbba3e00a847f9dc37aa0963da1
noahgribbin <noahgribbin@yahoo.com>
2015-10-01 11:56:27 -0700

    cleaning and adding document.getElementById

--- game.js
+++ game.js
@@ -1,24 +1,25 @@
 var name = prompt('What is your name?');
 var correctAnswers = 0	
+var one = document.getElementById('one');
+var two = document.getElementById('two');
+var three = document.getElementById('three');
+
 var gender = prompt('Am i a boy?');
 var genderCorrectAnswer = 'yes'
 gender=gender.toLowerCase()
-console.log(gender)
-
-if(gender === genderCorrectAnswer||gender ==='y'||gender ==='Y'){
+if(gender === genderCorrectAnswer||gender ==='y'){
 	correctAnswers += 1
-	alert('Good guess');
+	alert('HEYO, good guess!');
 }else if(gender === 'n'||gender ==='no'){
-	alert('hmm, not quite');	
+	alert('no, my hair\'s not that long is it?');	
 }
-
 
 var birthPlace = prompt('Was I born in seattle?');
 var birthPlaceCorrectAnswer = 'yes'
 birthPlace=birthPlace.toLowerCase()
-if(birthPlace === birthPlaceCorrectAnswer||birthPlace === 'y'||birthPlace === 'Y'){
+if(birthPlace === birthPlaceCorrectAnswer||birthPlace === 'y'){
 	correctAnswers += 1
-	alert('Good guess');
+	alert('how\'d you know?');
 }else if(birthPlace === 'n'||birthPlace ==='no'){
 	alert('hmm, not quite');
 }
@@ -26,11 +27,11 @@ if(birthPlace === birthPlaceCorrectAnswer||birthPlace === 'y'||birthPlace === 'Y
 var favoriteFood = prompt('Is Pho my favorite food?');
 var favoriteFoodCorrectAnswer = 'yes'
 favoriteFood=favoriteFood.toLowerCase()
-if(favoriteFood === favoriteFoodCorrectAnswer||favoriteFood === 'y'||favoriteFood === 'Y'){
+if(favoriteFood === favoriteFoodCorrectAnswer||favoriteFood === 'y'){
 	correctAnswers += 1
-	alert('Good guess');
+	alert('Correct! So yummy!');
 }else if(favoriteFood === 'n'||favoriteFood ==='no'){
-	alert('hmm, not quite');
+	alert('Of course it is!');
 }
 
 console.log('Question 1 response: ' + gender)
@@ -46,15 +47,8 @@ if(correctAnswers === 0){
 }else{
 	alert('Ya got em all!!!');
 }
-if(gender !== genderCorrectAnswer && gender !=='y'){
-	alert('Actually i am a boy');
-}
-if(birthPlace !== birthPlaceCorrectAnswer && birthPlace !== 'y'){
-	alert('Actually I was born in seattle');
-}
-if(favoriteFood !== favoriteFoodCorrectAnswer && favoriteFood !== 'y'){
-	alert('Actually my favorite food is Pho');
-}
+
 	alert('Thanks for playing my game ' + name +".");
 
-	
+
+
