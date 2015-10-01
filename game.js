bc7f5f66746b51eb1ae8ad9a55b319f9de953547
noahgribbin <noahgribbin@yahoo.com>
2015-09-30 19:19:58 -0700

    if/else logic properly implemented

--- game.js
+++ game.js
@@ -5,21 +5,12 @@ var correctAnswers = 0
 		if(gender === genderCorrectAnswer){
 			correctAnswers += 1
 			alert('Good guess');
-		}else if(gender === 'y'||gender ==='Y'||gender==='YES'){
-			alert('Error: Incorrect input.');
-		}else if(gender === 'n'||gender ==='N'||gender==='NO'){
-			alert('Error: Incorrect input.');
 		}
-
 	var birthPlace = prompt('Was I born in seattle?');
 		var birthPlaceCorrectAnswer = 'yes'
 			if(birthPlace === birthPlaceCorrectAnswer){
 				correctAnswers += 1
 				alert('Good guess');
-			}else if(birthPlace === 'y'||birthPlace ==='Y'||birthPlace==='YES'){
-				alert('Error: Incorrect input.');
-			}else if(birthPlace === 'n'||birthPlace ==='N'||birthPlace==='NO'){
-				alert('Error: Incorrect input.');
 			}
 
 	var favoriteFood = prompt('Is Pho my favorite food?');
@@ -27,14 +18,10 @@ var correctAnswers = 0
 			if(favoriteFood === favoriteFoodCorrectAnswer){
 				correctAnswers += 1
 				alert('Good guess');
-			}else if(favoriteFood === 'y'||favoriteFood === 'Y'||favoriteFood === 'YES'){
-				alert('Error: Incorrect input.');
-			}else if(favoriteFood === 'n'||favoriteFood ==='N'||favoriteFood==='NO'){
-				alert('Error: Incorrect input.');
 			}
-			console.log('Question 1 response:' + " " + gender)
-			console.log('Question 2 response:' + " " + birthPlace)
-			console.log('Question 3 response:' + " " + favoriteFood)
+	console.log('Question 1 response:' + " " + gender)
+	console.log('Question 2 response:' + " " + birthPlace)
+	console.log('Question 3 response:' + " " + favoriteFood)
 	if(correctAnswers === 0){
 		alert('Darn none right.');
 	}else if(correctAnswers === 1){
@@ -53,5 +40,4 @@ var correctAnswers = 0
 	if(favoriteFood !== favoriteFoodCorrectAnswer){
 		alert('Actually my favorite food is Pho');
 	}
-		alert('Thanks for playing my game ' + name +".");
-	//learning how to make a comment
+	
